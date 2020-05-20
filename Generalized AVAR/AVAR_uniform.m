f24ebe0bfb61d0d224c742c6389bdc276a9e8869
Hossein <haeri.hsn@gmail.com>
2020-05-19 21:42:22 -0400

    add some files

---
+++ Generalized AVAR/AVAR_uniform.m
@@ -0,0 +1,22 @@
+function [avar] = AVAR_uniform(t,x,tau)
+n = numel(x);
+avar = [];
+counter = 0;
+t_min = min(t);
+t_max = max(t);
+
+t_range = t_max-t_min;
+
+for i=1:numel(tau)
+    T = tau(i);
+    
+    avar_sum = 0;
+    
+    while j < n
+        t(j)
+    end
+    avar = [avar 0.5*avar_sum./counter];
+end
+
+
+
---
+++ Generalized AVAR/presentation_plots.m
@@ -0,0 +1,60 @@
+close all
+clear all
+clc
+set(groot,'defaulttextinterpreter','tex');
+set(groot, 'defaultAxesFontSize',12);
+N = 1;
+color = lines(N+1);
+num_samples = 100;
+t = linspace(1,num_samples,num_samples);
+% t = sort(rand(1,num_samples));
+
+
+q = floor(numel(t)/2);
+y = 0.05*t.^1.5;
+
+data = [];
+b = [10 20];
+for n= 1:N
+%    data = [data; y + b(n)*(rand(1,numel(t))-0.5)];
+    data = [data; y + b(n).*(normrnd(0,1,[1 numel(t)]))];
+end
+
+
+hold on
+
+c = lines(4);
+% for i= 1:num_samples
+%     xline(t(i),'-','Color',[.8 .8 .8],'LineWidth',1);
+% %     alpha(0.4)
+% end
+% 
+
+
+% for i= 1:num_samples
+%     xline(t(i),'-','Color',[.8 .8 .8],'LineWidth',1);
+% %     alpha(0.4)
+% end
+% 
+
+for i= 1:num_samples/10
+    k = i*10;
+    xline(t(k),'-','Color',[.8 .8 .8],'LineWidth',1);
+    
+    scatter(t(k)-5,mean(data(k-9:k)),50,'O','MarkerFaceColor',c(2,:),'MarkerEdgeColor',c(2,:));
+end
+
+
+
+scatter(t,data,10,'filled','MarkerFaceAlpha',0.8)
+
+
+
+
+set(gca,'xtick',[])
+set(gca,'xticklabel',[])
+set(gca,'ytick',[])
+set(gca,'yticklabel',[])
+
+xlabel('Sample #')
+ylabel('\theta')
